8d2cebecb15739dafa9e847d21b56e9d8caa9837
techzbq <zbq_930713@163.com>
2016-12-20 20:31:28 +0800

    fix error

--- Client_Module.cpp
+++ Client_Module.cpp
@@ -22,8 +22,7 @@ BOOST_PYTHON_MODULE(Client_Module)
                     .def("initialize", &MPI_Client::initialize)
                     .def("run", &MPI_Client::run)
                     .def("stop", &MPI_Client::stop)
-                    .def("finalize", &&MPI_Client::finalize)
+                    .def("finalize", &MPI_Client::finalize)
                     .def("send_action", &MPI_Client::send_action)
                     .def("set_wid", &MPI_Client::set_wid)
-
         }
