8c9f6597f41b6a0646fc41b658b9a10ed39e933f
techzbq <zbq_930713@163.com>
2017-01-09 14:03:58 +0800

    fix IRecv_handler_wrapper param problem

--- IRecv_handler_Wrapper.cpp
+++ IRecv_handler_Wrapper.cpp
@@ -9,12 +9,12 @@ struct IRecv_handler_Wrapper : IRecv_handler, wrapper<IRecv_handler>
 {
     void handler_recv_int(int mpi_tags, Pack_Int pack)
     {
-        this->get_override("handler_recv_int")();
+        this->get_override("handler_recv_int")(mpi_tags, pack);
     };
 
     void handler_recv_str(int mpi_tags, Pack_Str pack)
     {
-        this->get_override("handler_recv_str")();
+        this->get_override("handler_recv_str")(mpi_tags, pack);
     };
 };
 
